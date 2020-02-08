d2e6eb076090525c287c4f353feec3c01ec407c3
cipo64 <pietrobernaei@gmail.com>
2020-02-08 12:00:49 +0100

    risolto problema radice quadrata

--- capitolo2/radn.m
+++ capitolo2/radn.m
@@ -8,15 +8,16 @@ tolx=1/1000;
 if x<=0
     error('valore in ingresso errato');
 end
-x0=x/2;
+x1=x/2;
 for i=1:imax
+   x0=x1
    fx=x0^n-x;
-   fx1=(n-1)*x0^(n-1);
+   fx1=(n)*x0^(n-1);
    x1=x0-fx/fx1;
    if abs(x1-x0)<=tolx
        break
    end
-   x0=x1;
+   
 end
 if abs(x1-x0)>tolx
     error('metodo non converge')
