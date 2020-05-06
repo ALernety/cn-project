7490c7769d105347064b15cc55c1f8424e2f7448
niccoloPiazzesi <npiazzesi@gmail.com>
2020-05-06 12:04:14 +0200

    secanti dovrebbe essere ok

--- capitolo2/secanti.m
+++ capitolo2/secanti.m
@@ -1,20 +1,31 @@
-function [x,nit,tolf]=secanti(f,x0,x1,tolx,maxit)
-  nit=0;
+function [x, i]=secanti(f,x0,x1,tolx,maxit)
+   %
+       % [x,i] = secanti( f,  x0, x1, tolx [, maxit] )
+       %
+       % Metodo delle secanti per determinare una approssimazione
+       % della radice di f(x)=0 con tolleranza  tolx, a
+       % partire da x0, entro maxit iterationi (default = 100).
+  format long e
+  if nargin<4 
+    error('numero argomenti insufficienti');
+  elseif nargin==4
+    maxit = 100;
+  end
+  i=0;
   fx0=feval(f,x0);
-  err=abs(x1-x0);
-  while (nit<maxit) && (err>tolx)
+  for i=1:maxit
       fx1=feval(f,x1);
       dfx1=(fx1-fx0)/(x1-x0);
-      tolf=tolx*abs(dfx1);
-      if abs(fx1)<=tolf
-         break
+      x=x1-(fx1/dfx1);
+      if abs(x1-x0)<=tolx*(1+abs(x0))
+        break;
       end
-      x2=x1-(fx1/dfx1);
-      err=abs(x2-x1);
       x0=x1;
-      x1=x2;
+      x1=x;
       fx0=fx1;
-      nit=nit+1;
+      
   end
-  x=x1;
+  if abs(x-x0) > tolx*(1+abs(x0))
+    error('metodo non converge'); 
+  end
 end
