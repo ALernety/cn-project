482e9bf2b08180438372797355e53267cb6f0f30
BernabeiPietro <pietrobernaei@gmail.com>
2020-05-15 15:55:03 +0200

    aggiunto es 17-18-19

---
+++ capitolo4/es19.m
@@ -0,0 +1,21 @@
+a=-1;
+b=1;
+ntot=100;
+erreq=zeros(ntot-3,1);
+errch=zeros(ntot-3,1);
+for n=4:ntot
+xeq=linspace(a,b,n+1);
+xch=cos((2*linspace(0,n,n+1)*pi)/(2*n+2));
+xch(1)=a;
+xch(n+1)=b;
+xq=linspace(a,b,100);
+yeq=cos((pi*(xeq(:).^2))/2);
+ych=cos((pi*(xch(:).^2))/2);
+yq=cos((pi*(xq(:).^2))/2);
+riseq=spline(xeq,yeq,xq);
+risch=spline(xch,ych,xq);
+erreq((n-3),1)=norm(yq-riseq);
+errch((n-3),1)=norm(yq-risch);
+end
+x=(4:1:n);
+semilogy(x,erreq,x,errch);
--- capitolo4/splinenat.m
+++ capitolo4/splinenat.m
@@ -1,12 +1,29 @@
-function output=splinenat(X,Y,QX)
+function output=splinenat(x,y,xq)
 %
+% output=splinenat(x,y,xq)
+%funzione che calcola la spline cubica naturale.
+%Input: 
+%   x=vettore delle ascisse su cui calcolare la spline
+%   y=vettore dei valori di f(x), con x ascissa
+%   xq= insieme delle ascisse di cui si vuole sapere il valore della spline
+%Output:
+%   output=vettore delle approssimazioni sulle ascisse xq
 %
-%
-%
-%
-pp=spline0(X,Y);
-if nargin == 2
-    output = pp;
-else
-    output = ppval(pp,QX);%questo non funziona
+n=length(x);
+l=length(xq);
+if(length(y)~=n), error("dati in input con dimensioni differenti");end
+[x1,i]=sort(x);
+y1=y(i);
+m=spline0(x1,y1);
+h=diff(x1);
+df = diff(y1)./h;
+r=y(1 :(n-1))-((h(1:n-1).^2)/6)*m(2:n);
+q=df(1:n-1)-h(1:n-1)*(m(2:n)-m(1:n-1));
+output=zeros(l,1);
+for i=1:l
+  indg=find(xq(i)<=x1(2:n),1)+1;
+  indp=find(xq(i)>=x1(1:n-1),1,'last');
+  output(i)=(((xq(i)-x1(indp))^3)*m(indg)+((x1(indg)-xq(i))^3)*m(indp))/(6*h(indp))+q(indp)*(xq(i)-x1(indp))+r(indp);
+end
+return
 end
