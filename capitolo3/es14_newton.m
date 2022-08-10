8d7379dd666809b0e2f6e73dc0f223e14cc154b3
Ali Sha <alernety@gmail.com>
2022-08-10 17:47:48 +0200

    Refactoring, to use variables instead of array
    To use the jacobian function needs to have the syms, but not an array
    * change description in input secton for function
    * change function to use variables instead of array

--- capitolo3/es14_newton.m
+++ capitolo3/es14_newton.m
@@ -1,7 +1,12 @@
 function [x, nit] = newton(fun, jacobian, x0, tol, maxit)
     % [x, nit] = newton(fun, jacobian, x0, tol, maxit)
     % Newton's method, used to calculate non linear systems solutions
-    % Input:  fun       - array of functions
+    % Input:  fun       - array of functions, 
+    %                     should have input as variables, but not an array
+    %                     Working example:
+    %                         @(x1,x2) [x1-x2; x1+x2]
+    %                     Wrong example:
+    %                         @(x) [x(1)-x(2); x(1)+x(2)]
     %         jacobian  - jacobian matrix of the functions in variable 'fun'
     %         x0        - array of initail values
     %         tol       - tolerance                     (default = 10e-6)
@@ -17,16 +22,18 @@ function [x, nit] = newton(fun, jacobian, x0, tol, maxit)
         error('Tollerance is smaller then precision of the computer!');
     end
 
-    x = x0;
-    fun_x = feval(fun, x);
+    x = num2cell(x0);
+    fun_x = feval(fun, x{:});
 
     for nit = 1:maxit
-        jacobian_x = feval(jacobian, x);
+        jacobian_x = feval(jacobian, x{:});
         H = jacobian_x \ fun_x;
-        x = x - H;
-        fun_x = feval(fun, x);
+        x = num2cell(cell2mat(x) - H');
+        fun_x = feval(fun, x{:});
         % norm of matrix 'fun_x' corresponds to the precison of solution
         if norm(fun_x) < tol, break, end
     end
+
+    x = cell2mat(x);
 
 end
